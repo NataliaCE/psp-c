194fb72470f6137884f767a68fc9951088df3044
Natalia <200351@iessierradeguara.com>
2021-11-09 11:58:45 +0100

    NOV 09

--- prog12-productorSeparado/productor.c
+++ prog12-productorSeparado/productor.c
@@ -1,6 +1,7 @@
 //importar los módulos .h que necesitemos.
 //si necesitamos funciones.h también, y entre comillas.
 #include <stdio.h>
+#include <signal.h>
 
 #include <funciones.h>
 
@@ -10,6 +11,8 @@
 // o una común que discrimine por el código de señal
 // que viene en el parámetro.
 
+void gestor_senyal(int senyal);
+
 //implementar el main.
 //en este caso, el main recibirá como parámetro
 //el pid del consumidor 
@@ -18,4 +21,11 @@ int main(int argc, char const *argv[])
 {
     printf("El parámetro es %s, pero no es de tipo pid_t, así que lo tengo que convertir. atoi (o mejor atol) puede ser una buena opción.",argv[1]);
     return 0;
+}
+
+void gestor_senyal(int senyal){
+    if(senyal == SIGUSR2){ //La señal 2 la usaremos para finalizar.
+        final = true;
+    }
+    //cualquier otra señal que hayamos asociado a la función, no hará nada.
 }
